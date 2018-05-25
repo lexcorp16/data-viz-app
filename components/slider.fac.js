b5bb6e6676e16e2524c55360fe8c50653666b430
Fasoro-Joseph <a.fasorojoseph@jaramobile.com>
2018-05-25 23:51:19 +0100

    Create data visualization app

---
+++ components/slider.fac.js
@@ -0,0 +1,34 @@
+(() => {
+  'use strict';
+
+  const today = new Date();
+  const date = `${today.getFullYear()}-0${today.getMonth() +
+    1}-${today.getDate()}`;
+
+  const url = `https://studio-api.paystack.co/insights/spenders?from=2017-01-01&to=${date}`;
+  const secretKey = 'sk_test_584bfc762c9d0eeb4f4dc722912f3b22f3c4c925';
+
+  angular.module('ngApp').factory('sliderFactory', $http => {
+    $http.defaults.headers.common['Authorization'] = 'Bearer ' + secretKey;
+
+    const getData = () => $http.get(url);
+
+    const calculateTotalSpend = data => {
+      let total = 0;
+      for (const datum of data) {
+        total += datum.total_transaction_amount;
+      }
+      return total;
+    };
+
+    const getGroupOfCustomers = (data, value) => {
+      return data.slice(0, value);
+    };
+
+    return {
+      getData,
+      calculateTotalSpend,
+      getGroupOfCustomers,
+    };
+  });
+})();
